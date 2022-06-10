86ac359b08f3add5a2673e5e1637dc9be98780dc
AntonioDelGreco <ddiegoantonio1@gmail.com>
2022-06-09 22:55:28 -0300

    5

--- py.py
+++ py.py
@@ -1,120 +1,143 @@
-#BIBLIOTECA VIRTUAL
+from os import system
 
-sesiones = ['maria','jose','marcelo']
-agendaLibros = {'harry potter' : {'Nombre': 'harry potter', 'Autor': 'jose', 'Generos': 'fantasia'}, 'merlin' : {'Nombre': 'merlin', 'Autor': 'maria', 'Generos': 'fantasia'}}
+dataLibros = []
 
-def biblioteca():
+def extraccionDatos():
+    data = ''
+    archivo = open('archivo.txt', 'a+')
+    archivo.seek(0)
+    data = archivo.readlines()
+    archivo.close()
+    return data
+
+def inscripcion(nombre):
+    for i in extraccionDatos():
+        if nombre in i:
+            return True
+
+def reseña ():
+    print ('1) Buscar Reseña')
+    print ('2) Agregar Reseña')
+    eleccion = int(input('¿Eleccion?:'))
+
+    if eleccion == 1:
+        nombre_libro = input ('Nombre del libro:')
+        for i in x.keys():
+            if nombre_libro == i:
+                print (nombre_libro, x.values())
+              
+    elif eleccion == 2:
+
+        reseña = input('Escriba su reseña:')
+        libro = input('¿Nombre del libro?:')
+        reseñas[libro] = reseña
+
+def buscarlibro ():
+    archivo = open('archivo.txt', 'a+')
+    print ('1: Buscar por Autor')
+    print ('2: Buscar por Nombre')
+    print ('3: Buscar por Genero')
+    print ('4: Buscar por Precio')
+    eleccion = int (input ('Elija una opcion: '))
+ 
+    if eleccion == 1:  
+        nombreAutor = input ('Introducir nombre del autor: ')
+ 
+        archivo.seek(0)
+        for i in archivo:
+            if nombreAutor in i:
+                nombreLibro, autor, generos, precio = i.strip().split('; ')
+                print(f'\nNombre: {nombreLibro}\nAutor: {autor}\nGeneros: {generos}\nPrecio: {precio}\n')
+            input()
+ 
+    elif eleccion == 2:
+        nombredelLibro = input ('Introducir nombre del libro: ')
+ 
+        archivo.seek(0)
+        for i in archivo:
+            if nombredelLibro in i:
+                nombreLibro, autor, generos, precio = i.strip().split('; ')
+                print(f'\nNombre: {nombreLibro}\nAutor: {autor}\nGeneros: {generos}\nPrecio: {precio}\n')
+            input()
+ 
+    elif eleccion == 3:
+        genero1 = input ('Introducir genero 1:')
+        genero2 = input ('Introducir genero 2:')
+        genero3 = input ('Introducir genero 3:')
+ 
+        archivo.seek(0)
+        for i in archivo:
+            if genero1 in i:
+                nombreLibro, autor, generos, precio = i.strip().split('; ')
+                print(f'\nNombre: {nombreLibro}\nAutor: {autor}\nGeneros: {generos}\nPrecio: {precio}\n')
+    archivo.close()
+    
+def agregarlibro ():
+        archivo = open('archivo.txt', 'a+')
+        nombre = input('Nombre del libro: ')
+        dataLibros.append(nombre)
+        dataLibros.append(input('Autor/es del libro: '))
+        dataLibros.append(input('Genero/s del libro (si es nas de uno separarlo con ","): '))
+        dataLibros.append(input ('Precio que al que quiere venderlo? '))
+        
+        if inscripcion(nombre):
+            print('Su libro ya esta registrado, ingrese uno distinto')
+            input()
+        else:
+            formato = '; '.join(dataLibros)
+            archivo.write(formato + '\n')
+            archivo.close() 
+    
+def catalogo ():
+    dataLibros = extraccionDatos()
+    for i in dataLibros:
+        nombreLibro, autor, generos, precio = i.strip().split('; ')
+        print(f'\nNombre: {nombreLibro}\nAutor: {autor}\nGeneros: {generos}\nPrecio: {precio}\n')
+    input()   
+
+def eliminarLibro():
+    dataLibros = extraccionDatos()
+    archivo = open('archivo.txt', 'w')
+    libroBuscado = input('Ingrese el nombre del libro que desea eliminar: ')
+    for i in dataLibros:
+        if libroBuscado in i:
+            dataLibros.remove(i)
+            for j in dataLibros:
+                archivo.write(j)
+            break
+    archivo.close()
+        
+while True:
+    system('cls')
+    dataLibros.clear()
     print('\n+---+---+---+---+---+---+---+---+---+---+\n| B | i | b | l | i | o | t | e | c | a |\n+---+---+---+---+---+---+---+---+---+---+\n')
     print('------------------------------------------')
     print('Opcion 1: Buscar Libro')
-    print('Opcion 2: Agregar Libro')
-    print('Opcion 3: Eliminar Libro')
-    print('Opcion 4: Catalogo')
-    print('Opcion 5: Agregar Reseña')
-    print('Opcion 6: Puntuar Libro')
-    print('Escribir cualquier otra opcion, cierra el programa.')
-    opcion = int(input ('Elegir Opcion: '))
+    print('Opcion 2: Comprar un libro')
+    print('Opcion 3: Agregar Libro, para vender')
+    print('Opcion 4: Eliminar Libro')
+    print('Opcion 5: Catalogo')
+    print('Opcion 6: Reseñas')
+    print('Opcion 7: Puntuar Libro')
+    print('Opcion 8: Salir')
     print('------------------------------------------')
-    opciones(opcion)
+    opcion = int(input ('\nElegir Opcion: '))
 
-def opciones(x):
-    if x == 1:
+    if opcion == 1:
         buscarlibro()
-    
-    elif x == 2:
+    elif opcion == 2:
+        #compraLibro()
+        merca = 1
+    elif opcion == 3:
         agregarlibro ()
-
-    elif x == 3:
-       eliminarLibro(agendaLibros)
-
-    elif x == 4:
-        mostrarAgenda(agendaLibros)
-
-def buscarlibro ():
-    print ('1: Buscar por autor')
-    print ('2: Buscar por nombre')
-    print ('3: Buscar por genero')
-    eleccion = int (input ('Elija una opcion: '))
-
-    if eleccion == 1:  
-        nombreautor = input ('Introducir nombre del autor: ')
-        for i in agendaLibros:
-            for j in agendaLibros[i].values():
-                if nombreautor in j:
-                   mostrarAgenda(i)
-                else:
-                    print('Autor no registrado')
-    elif eleccion == 2:  
-        nombre = input ('Introducir nombre del libro: ')
-        for i in agendaLibros:
-            for j in agendaLibros[i].values():
-                if nombre in j:
-                   mostrarAgenda(i)
-                else:
-                    print('Libro no registrado')
-    elif eleccion == 3:  
-        genero = input ('Introducir el genero: ')
-        for i in agendaLibros:
-            for j in agendaLibros[i].values():
-                if genero in j:
-                   mostrarAgenda(i)
-                else:
-                    print('Autor no registrado')
-    
-def agregarlibro ():
-    nombreLibro = input ('Nombre del libro: ')
-    autor = input ('Autor/es del libro: ')
-    generos = input ('Genero/s del libro (si es nas de uno separarlo con ","): ')  
-
-    if agendaLibros.get(nombreLibro) == None:
-        agendaLibros[nombreLibro] = {'Nombre': nombreLibro, 'Autor': autor, 'Generos': generos}  
-    else:
-        print('El libro ya esta registrado')
-
-def mostrarAgenda (x):
-    for i in x:
-        for j,k in x[i].items():
-            print(j, ':', k)
-        print('')
-    input()    
-
-def eliminarLibro(x):
-    libroBuscado = input('Ingrese el nombre del libro que desea eliminar: ')
-    for i in x:
-        if i == libroBuscado:
-            x.pop(i)
-            break
-    mostrarAgenda()
-  
-def registro():
-    usuario = input('Ingrese su nombre de usuario: ')
-    sesiones.append(usuario)
-                
-def listaUsuarios():
-    for i in sesiones:
-        print(i)
-
-def eliminarUsuario():
-    usuario = input('\nQue usuario desea eliminar? ')
-    for i in sesiones:
-        if i == usuario:
-            sesiones.remove(usuario)
-    
-  
-while True:
-               
-    opc = int(input(('\n1) Ir a la biblioteca\n2) Registrarse\n3) Ver que usuarios existen\n4) Eliminar su usuario\n5) Salir\n\topcion? ')))
-    if opc == 1:
-        biblioteca()
-    elif opc == 2:
-       registro()
-    elif opc == 3:
-        listaUsuarios()
-    elif opc == 4:
-        eliminarUsuario()
-    elif opc == 5:
+    elif opcion == 4:
+        eliminarLibro()
+    elif opcion == 5:
+        catalogo()
+    elif opcion == 6:
+        reseña()
+    elif opcion == 7:
+        hola = 1
+    elif opcion == 8:
         break
-        
-
-
 
